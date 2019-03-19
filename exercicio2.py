8c430fa2dde4f700b5980de96e59296233fc99f6
lc-rodrigues <48491937+lc-rodrigues@users.noreply.github.com>
2019-03-19 17:53:25 -0300

    Delete exercicio2.py

--- exercicio2.py
+++
@@ -1,9 +0,0 @@
-vsom = 343        #velocidade do som em m/s
-vluz = 300000000  #velocidade da luz em m/s
-t = 3             #tempo levado para escutar o som
-d = 3*(vsom*vluz)/(vluz-vsom)  #distância entre o fogo de artifício e o observador
-
-print(' ')
-print('Exercício 2')
-print(' ')
-print('A distância entre o fogo de artifício e o observador, em metros, é ', d)
