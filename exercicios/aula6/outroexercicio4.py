5d90a35c34392d153604d2dda348e2a9091f5f08
lc-rodrigues <48491937+lc-rodrigues@users.noreply.github.com>
2019-04-03 13:07:34 -0300

    Update outroexercicio4.py

--- exercicios/aula6/outroexercicio4.py
+++ exercicios/aula6/outroexercicio4.py
@@ -2,16 +2,17 @@ import turtle
 import math
 
 def circle(t, r):
-    t.right(90)
-    t.forward(r)
+    t.forward(r)                            #desenhar o raio e posicionando o cursor
     t.left(90)
-    for i in range(360):
-        t.left(1)
-        t.forward(2*math.pi*r/360)
+    for i in range(90):                     #loop para desenhar o círculo
+        t.left(4)
+        t.forward(2*math.pi*r/90)
+    t.left(90)                              #reposicionar o cursor na posição inicial
+    t.forward(r)
+    t.left(180)
         
 janela = turtle.Screen()
 joana = turtle.Turtle()
-circle(joana, 100)
+circle(joana, 200)
 
 janela.mainloop()
-    
