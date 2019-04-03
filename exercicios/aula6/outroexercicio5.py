2a2b533b5d81503bbacb4a5c23dec57afc7634a6
lc-rodrigues <48491937+lc-rodrigues@users.noreply.github.com>
2019-04-03 13:08:17 -0300

    Update outroexercicio5.py

--- exercicios/aula6/outroexercicio5.py
+++ exercicios/aula6/outroexercicio5.py
@@ -1,17 +1,18 @@
 import turtle
 import math
 
-def arc(t, r, angle):
-    t.right(90)
-    t.forward(r)
+def arc(t, r, angle):                   
+    t.forward(r)                           #desenhar o raio e posicionar o cursor
     t.left(90)
-    for i in range(360):
+    for i in range(angle):                 #loop para desenhar o arco de círculo
         t.left(1)
         t.forward(2*math.pi*r/360)
+    t.left(90)                             #reposicionar o cursor na posição inicial
+    t.forward(r)
+    t.left(180-angle)
         
-janela = turtle.Screen()
+janela = turtle.Screen()                  
 joana = turtle.Turtle()
-arc(joana, 100)
+arc(joana, 150, 330)
 
 janela.mainloop()
-    
